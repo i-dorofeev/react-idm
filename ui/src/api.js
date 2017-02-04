ff3f7fcd4272c3b7a1ac32f34740d896afc1ac13
Ilya Dorofeev <i.dorofeev@solarsecurity.ru>
2017-02-04 22:52:45 +0300

    Refactored api.js

--- ui/src/api.js
+++ ui/src/api.js
@@ -1,21 +1,29 @@
 function url(path) {
-    return 'http://localhost:8080/' + path;
+    return 'http://localhost:8080' + path;
 }
 
-export function fetchPeople() {
-    return fetch(url('people'))
-        .then(response => response.json())
+function get(url) {
+    return fetch(url)
+        .then(response => response.json());
 }
 
-export function addNewPerson(firstName, lastName) {
-    return fetch(url('people'), {
+function put(url, payload) {
+    return fetch(url, {
         method: 'put',
         headers: new Headers({
             'Content-Type': 'application/json'
         }),
-        body: JSON.stringify({
-            firstName: firstName,
-            lastName: lastName
-            })
+        body: JSON.stringify(payload)
+    });
+}
+
+export function fetchPeople() {
+    return get(url('/people'));
+}
+
+export function addNewPerson(firstName, lastName) {
+    return put(url('/people'), {
+        firstName: firstName,
+        lastName: lastName
     });
 }
